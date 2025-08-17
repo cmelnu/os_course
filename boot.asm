cd037e73dd0a00c520e64d26b48d005540a324d9
cmelnu <cmn263@gmail.com>
2025-08-17 21:49:00 +0200

    Section 3, chapter 8 and 13: improved the bootloader and implemented two interrupts in real mode

--- boot.asm
+++ boot.asm
@@ -1,7 +1,47 @@
-ORG 0x7c00
+ORG 0x0
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+times 33 db 0      ; Reserve 33 bytes for BIOS Parameter
+
 start:
+    jmp 0x7c0:step2    ; Jump to the 'step2' label at segment 0x7c0 (bootloader)
+
+handle_zero:
+    mov ah, 0eh
+    mov al, 'A'
+    mov bx, 0x00
+    int 0x10
+    iret
+
+handle_one:
+    mov ah, 0eh
+    mov al, 'V'
+    mov bx, 0x00
+    int 0x10
+    iret
+
+step2:
+    cli                 ; Disable interrupts (clear interrupt flag)
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti                 ; Enable interrupts (set interrupt flag)
+
+    mov word[ss:0x00], handle_zero   ; Store the address of handle_zero at offset 0x00 in the stack segment
+    mov word[ss:0x02], 0x7c0
+
+    mov word[ss:0x04], handle_one    ; Store the address of handle_one at offset 0x00 in the stack segment
+    mov word[ss:0x06], 0x7c0
+
+    int 1    ; Software interrupt 1 (calls interrupt vector 1, typically the divide-by-zero handler). This will jump to ss:0x04
+
     mov si, message
     call print
     jmp $
@@ -9,7 +49,9 @@ start:
 print:
     mov bx, 0
 .loop:
-    lodsb              ; Load the character SI is pointing to
+    lodsb               ; Load the byte at [SI] into AL, then increment SI
+                        ; lodsb copies the value pointed to by SI into AL, then increases SI by 1.
+                        ; So: source pointer is SI, destination register is AL.
     cmp al, 0
     je .done           ; If the character is null, we are done
     call print_char    ; Print the character
